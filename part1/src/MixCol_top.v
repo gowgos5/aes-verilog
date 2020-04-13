dec6b8e46b77bc912357f0631158e8f9f6586f58
gowgos5 <gowgos5@gmail.com>
2020-04-13 15:29:53 +0800

    Add MixColumns

--- part1/src/MixCol_top.v
+++ part1/src/MixCol_top.v
@@ -1,7 +1,23 @@
 module MixCol_top(
-input  [127:0] ip,
-input  enable,
-output [127:0] op
+    input   [127:0] ip,
+    input   enable,
+    output  [127:0] op
 );
-////enter your code here
+
+wire [127:0] out;
+
+/* Instantiate 4 matrix_mult units */
+genvar i;
+generate
+    for (i=0; i<4; i=i+1)
+    begin: GEN_MULT
+        matrix_mult mult(
+            .ip(ip[i*8+:8]),
+            .op(out[i*8+:8])
+        );
+    end
+endgenerate
+
+assign op = enable ? out : ip;
+
 endmodule
