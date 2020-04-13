fffe4cccbaea7100a9ce97216a66fd723c25d908
gowgos5 <gowgos5@gmail.com>
2020-04-13 17:40:01 +0800

    Add Controller

--- part1/src/AEScntx.v
+++ part1/src/AEScntx.v
@@ -1,19 +1,43 @@
 module AEScntx(
-//from testbench
-input  clk,
-input  start,
-input  rstn,
-//to AEScore
-output  accept,
-output  [3:0] rndNo,
-output  enbSB,
-output  enbSR,
-output  enbMC,
-output  enbAR,
-output  enbKS,
-//to testbench
-output  done,
-output  [9:0] completed_round
+    // from testbench
+    input   clk,
+    input   start,
+    input   rstn,
+
+    // to AEScore
+    output  accept,
+    output reg [3:0] rndNo = 0,
+    output  enbSB,
+    output  enbSR,
+    output  enbMC,
+    output  enbAR,
+    output  enbKS,
+
+    // to testbench
+    output reg done = 0,
+    output  [9:0] completed_round
 );
-////enter your code here
+
+always @ (posedge clk)
+begin
+    if (~rstn)
+    begin
+        rndNo <= 0;
+        done <= 0;
+    end
+    else if (start)
+    begin
+        rndNo <= (rndNo < 10) ? (rndNo + 1) : 0;
+        done <= (rndNo == 10);
+    end
+end
+
+assign enbSB = (rndNo >= 1) && (rndNo <= 10);
+assign enbSR = (rndNo >= 1) && (rndNo <= 10);
+assign enbMC = (rndNo >= 1) && (rndNo <= 9);
+assign enbAR = (rndNo >= 0) && (rndNo <= 10);
+assign enbKS = (rndNo >= 1) && (rndNo <= 10);
+assign accept = (rndNo == 0);
+assign completed_round = (rndNo == 0) ? 0 : (1 << rndNo);
+
 endmodule
