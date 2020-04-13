cade0cf0b00cd0795481a65ac9ef56fa4ebbd8c5
gowgos5 <gowgos5@gmail.com>
2020-04-13 19:42:59 +0800

    Add constraint and synthesis scripts

--- part1/syn/constraint.tcl
+++ part1/syn/constraint.tcl
@@ -1,4 +1,11 @@
-#create a clock with given constrained clock period
-#create input delay on all inputs except clock
-#create output delay on all outputs
-#set given load capacitance on all output pins
+# Clock period = 2 ns
+create_clock -period 2 [get_ports clk]
+
+# Input delay = 0.2 ns
+set_input_delay 0.2 -clock clk [remove_from_collection [all_inputs] [get_ports clk]]
+
+# Output delay = 0.2 ns
+set_output_delay 0.2 -clock clk [all_outputs]
+
+# Load capacitance = 5 fF
+set_load 5 [all_outputs]
--- part1/syn/synthesis.tcl
+++ part1/syn/synthesis.tcl
@@ -1,15 +1,18 @@
-#enter the files to be analyzed
-#elaborate top
-#source constraint file
-#compile the top module
-#report critical path
-#report area
+# analyse HDL source files
+analyze -format verilog {../src/aes_sbox.v ../src/subBytes_top.v ../src/shiftRows_top.v ../src/matrix_mult.v ../src/MixCol_top.v ../src/AddRndKey_top.v ../src/KeySchedule_top.v ../src/AESCore.v ../src/AEScntx.v ../src/AES_top.v}
 
+# elaborate top
+elaborate AES_top -architecture verilog
 
+# source constraint file
+source constraint.tcl
 
+# compile top module
+compile
 
+# report critical path
+redirect -tee report_timing_setup.txt {report_timing -delay max}
+redirect -tee report_timing_hold.txt {report_timing -delay min}
 
-
-
-
-
+# report area
+redirect -tee report_area.txt {report_area}
